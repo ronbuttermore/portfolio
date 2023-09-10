ef571d9f531e8c528fa54d9df83097fbe7c2b07c
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-10 16:12:09 -0600

    added screenshot

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
+const screenshot = require('./screenshot-main-page.png');
+
 export default function Project(props) {
   const projectStyle = {
     width: '18rem',
   };
 
-  const randomWidth = () => {
-    const number = Math.random() * (300 - 200) + 200;
-    return number.toString().split('.')[0];
-  };
-
   return (
     <div className="container">
       <div className="card" style={projectStyle}>
-        <img
-          className="card-img-top"
-          src={`http://placekitten.com/${randomWidth()}`}
-          alt="Card cap"
-        />
-        <div className="card-body">
+        <img className="card-img-top" src={screenshot} alt="Card cap" /> <div className="card-body">
         <h5 className="card-title">Name: {props.name}</h5>
           <p className="card-text">Description: {props.description}</p>
           <p className="card-text">ID: {props.id}</p>
