83bfd0fdc39c45b293b658a577d163548e5dca2f
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-11 05:42:01 -0600

    more styling and bug fixes

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,14 +4,16 @@ import './AboutMe.css'
 
 function AboutMe() {
     return (
-        <div class="aboutMe row">
-            <div class="col-lg-3 sidebar" align='center'>
-            <h2 id="heading">About Me: </h2>
-            <img src={headshot} alt ="Me, the creator of this web page" id="profile"></img>
-            </div>
-            <div class="col-lg-7 align-middle">
-            <p id="blurb">Hello! My name is Ron Buttermore. I am an aspiring programmer and a lover of all things creative!</p>
-            <p>Thanks for stopping by! There's lots of great things to come.</p>
+        <div class="container-fluid">
+            <div class="aboutMe row">
+                <div class="col-lg-3 col-sm-12 sidebar" align='center'>
+                <h2 id="heading">About Me: </h2>
+                <img src={headshot} alt ="Me, the creator of this web page" id="profile"></img>
+                </div>
+                <div class="col-lg-9 col-sm-12 align-middle">
+                <p id="blurb">Hello! My name is Ron Buttermore. I am an aspiring programmer and a lover of all things creative!</p>
+                <p>Thanks for stopping by! There's lots of great things to come.</p>
+                </div>
             </div>
         </div>
     );
